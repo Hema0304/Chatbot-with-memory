2ba52e66868db7b06127c1e025d3d54c09255383
Hema0304 <hemaselvam0304.july@gmail.com>
2026-05-02 12:54:43 +0530

    Update app.py

--- chatbot-ollama/app.py
+++ chatbot-ollama/app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #langchain tracking
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Simple Q&A chatbot using OLLAMA"
 
